bed9f2754873bcb0e54bceedb0f2e558dc48a90a
MidFag <MidFag@gmail.com>
2017-09-05 00:52:05 +0300

    Servelat_first_commit

---
+++ src/com/midfag/ClientSumm.java
@@ -0,0 +1,27 @@
+package com.midfag;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ClientSumm")
+public class ClientSumm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().append("Сумма клиента: "+(int)(Math.random()*100000));
+	}
+
+}
+
+
---
+++ src/com/midfag/ClientSummAll.java
@@ -0,0 +1,27 @@
+package com.midfag;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ClientSummAll")
+public class ClientSummAll extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().append("Сумма всех счетов клиента: "+(int)(Math.random()*1000000));
+	}
+
+}
+
+
